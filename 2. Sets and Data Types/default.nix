90a311f4905aa47a8c7e5bfdbba185b7d5d4bdaa
WeebNetsu <stephenvdw7777@gmail.com>
2026-04-27 11:25:46 +0200

    Complete sets tutorial

--- 2. Sets and Data Types/default.nix
+++ 2. Sets and Data Types/default.nix
@@ -6,7 +6,7 @@
   integer = 55;
   float = 3.1415;
   bool = false;
-  # notice how some keywords are allowed as names here
+  # notice how some keywords are allowed as names here, however, others like `let` are not allowed
   null = null;
   list = [
     9
@@ -30,4 +30,86 @@
     # also * is times, / divide, + add, - subtract
     distance = speed * time;
   };
+
+  let-example =
+    let # like defining variables
+      var = 2;
+    in
+    # note that the `var` variable is only available inside this `in` statement
+    var;
+
+  # not available as the in statement has ended (;)
+  # letOutsideExample = var;
+
+  bigger-let-example =
+    let
+      # can hold multiple variables
+      tickets = 270;
+      ticketPrice = 150;
+    in
+    tickets * ticketPrice;
+
+  set-let-example =
+    let
+      # it can of course also hold sets
+      grades = {
+        one = 50;
+        two = 20;
+        three = 30;
+        four = 60;
+        five = 89;
+        six = 72;
+        seven = 34;
+        eight = 100;
+        nine = 89;
+        ten = 58;
+        eleven = 47;
+      };
+    in
+    # can also hold sets we use a recursive set just so we can mix and match our skills a bit,
+    # does not need to be a recursive set can be a regular set
+    rec {
+      highest-grade = grades.eight;
+      lowest-grade = grades.two;
+      average =
+        # with allows us to exclude the `grades` attributes, so we don't have to type out grades.one grades.two
+        with grades; (one + two + three + four + five + six + seven + eight + nine + ten + eleven) / 11;
+      range = highest-grade - lowest-grade;
+    };
+
+  inheritance-example =
+    let
+      age = 25;
+      name = "Mike";
+      hobby = "gaming";
+      features = {
+        rich = true;
+        athletic = false;
+      };
+      skills = {
+        math = true;
+        science = false;
+        geography = true;
+        computer-science = true;
+        spanish = false;
+      };
+    in
+    {
+      # inherit allows us to assign a value from something in the existing scope with the same name
+      inherit age name hobby; # shorthand for age = age; name = name; hobby = hobby;
+      # inherit from a set, but don't include the set
+      inherit (features) rich athletic; # shorthand for rich = features.rich; athletic = features.athletic;
+
+      best-skills =
+        let
+          # you can also use inherit inside of let
+          inherit (skills) math geography computer-science;
+        in
+        {
+          can-calculate = math;
+          can-read-maps = geography;
+          can-code = computer-science;
+        };
+    };
+
 }
